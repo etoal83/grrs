b35caf88204bda3e6f63d70ed868ce9598a0c09b
etoal83 <koki.okutomi@gmail.com>
2020-12-29 01:11:55 +0900

    Add unit test of find_matches

--- src/main.rs
+++ src/main.rs
@@ -27,3 +27,10 @@ fn main() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem_ipsum\n");
+}
