9d4769aee82b208ebbf18afdf46098942cf0ffe1
etoal83 <koki.okutomi@gmail.com>
2020-12-29 01:11:10 +0900

    Extract find_matches from main

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,22 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line)?;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
     
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let _ = find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
 }
