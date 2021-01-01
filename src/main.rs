a200d1eb2b0803b618bb8fe2bb24ce5c92f79704
etoal83 <etoal83@gmail.com>
2021-01-02 01:13:25 +0900

    Extract find_matches and its unit test from main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,12 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line)?;
-        }
-    }
-
-    Ok(())
-}
-
 fn main() -> Result<()> {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
     
-    let _ = find_matches(&content, &args.pattern, &mut std::io::stdout());
+    let _ = grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem_ipsum\n");
 }
