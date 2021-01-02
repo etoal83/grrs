a469977046ea5b48d60fbdc1487416d68be45200
etoal83 <etoal83@gmail.com>
2021-01-02 14:50:38 +0900

    Add test case in cli.rs: find_content_in_file

--- tests/cli.rs
+++ tests/cli.rs
@@ -13,3 +13,20 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     
     Ok(())
 }
+
+use std::io::Write;
+use tempfile::NamedTempFile;
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+       .success()
+       .stdout(predicate::str::contains("test\nAnother test"));
+
+    Ok(())
+}
